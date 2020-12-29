6ea1c3560f231b0196cca28f733d460b051d86a4
Het Suthar <hetmewada028@gmail.com>
2020-12-29 15:32:32 +0530

    Signup UI and ViewHolder Created

---
+++ app/src/main/java/com/project/farmingapp/view/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.project.farmingapp.view.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.farmingapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
