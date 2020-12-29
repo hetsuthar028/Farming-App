97959bf6b013daa51cb06c1bf0354e74c1541b84
Het Suthar <hetmewada028@gmail.com>
2020-12-29 14:22:35 +0530

    Login Activity Created

--- app/src/main/java/com/project/farmingapp/view/auth/LoginActivity.kt
+++ app/src/main/java/com/project/farmingapp/view/auth/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.project.farmingapp
+package com.project.farmingapp.view.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.farmingapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
