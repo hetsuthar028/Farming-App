6fbdfbdf308e5232dcb21c15df2065e38671e179
Bharat Sirvi <sirvi3014@gmail.com>
2020-12-29 14:38:15 +0530

    dependency added

--- app/src/main/java/com/project/farmingapp/view/auth/LoginActivity.kt
+++ app/src/main/java/com/project/farmingapp/view/auth/LoginActivity.kt
@@ -8,5 +8,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
