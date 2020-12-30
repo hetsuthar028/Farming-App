ce951b0a226e89fea3f3a801c8d08521368105bc
Het Suthar <hetmewada028@gmail.com>
2020-12-30 09:18:30 +0530

    Login To Signup

--- app/src/main/java/com/project/farmingapp/view/auth/LoginActivity.kt
+++ app/src/main/java/com/project/farmingapp/view/auth/LoginActivity.kt
@@ -1,15 +1,21 @@
 package com.project.farmingapp.view.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.farmingapp.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
+        loginBtnLogin.setOnClickListener {
+            Intent(this, SignupActivity::class.java).also {
+                startActivity(it)
+            }
+        }
 
     }
 }
